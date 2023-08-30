639dec26edffde3c6b1f0eb6f7e32c0a77b3534e
bruno0pizzatto <bruno0pizzatto@gmail.com>
2023-08-30 19:12:18 -0300

    build: add logical operators

---
+++ StructuredProgramming/LogicalOperators.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fundamentals.StructuredProgramming
+{
+    public class LogicalOperators
+    {
+        public LogicalOperators()
+        {
+            testLogicalOperators()
+        }
+
+        public void testLogicalOperators()
+        {
+            // logical operators = Can be used to check if more than 1 condition is true/false
+
+            // && (AND)
+            // || (OR)
+
+            Console.WriteLine("What's the temperature outside: (C)");
+            double temp = Convert.ToDouble(Console.ReadLine());
+
+            if (temp >= 10 && temp <= 25)
+            {
+                Console.WriteLine("It's warm outside!");
+            }
+            else if (temp <= -50 || temp >= 50)
+            {
+                Console.WriteLine("DO NOT GO OUTSIDE!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
