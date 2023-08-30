01feecb9db166e8a162558106ea3ebf39efa60e6
bruno0pizzatto <bruno0pizzatto@gmail.com>
2023-08-30 19:16:26 -0300

    build: add ForEachLoopStatment

---
+++ StructuredProgramming/ForEachLoopStatment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fundamentals.StructuredProgramming
+{
+    public class ForEachLoopStatment
+    {
+        public ForEachLoopStatment()
+        {
+            test();
+        }
+
+        public void test()
+        {
+            // foreach loop = a simpler way to iterate over an array, but it's less flexible
+
+            String[] cars = { "BMW", "Mustang", "Corvette" };
+
+            foreach (String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
