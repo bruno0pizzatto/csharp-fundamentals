16a38b2315c8b2c0021e081dc99e9d77b64014c8
bruno0pizzatto <bruno0pizzatto@gmail.com>
2023-09-04 08:59:04 -0300

    build: add challenges HypotenuseCalculator

---
+++ Challenges/HypotenuseCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fundamentals.Challenges
+{
+    public class HypotenuseCalculator
+    {
+        public HypotenuseCalculator()
+        {
+            calculateHypotenuse();
+        }
+
+        public void calculateHypotenuse()
+        {
+            Console.WriteLine("Enter side A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt((a * a) + (b * b));
+
+            Console.WriteLine("The hypotenuse is: " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
