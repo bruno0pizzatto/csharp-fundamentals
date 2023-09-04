ebd83c92872ff0578999e7a3ba29b7a035c3e6cb
bruno0pizzatto <bruno0pizzatto@gmail.com>
2023-09-04 09:01:18 -0300

    build: add MathClassMethods

---
+++ MathClassMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fundamentals
+{
+    public  class MathClassMethods
+    {
+        public MathClassMethods()
+        {
+            test();
+        }
+
+        public void test()
+        {
+            double x = 3;
+            double y = 5;
+
+            double a = Math.Pow(x, 2);
+            double b = Math.Sqrt(x);
+            double c = Math.Abs(x);
+            double d = Math.Round(x);
+            double e = Math.Ceiling(x);
+            double f = Math.Floor(x);
+            double g = Math.Max(x, y);
+            double h = Math.Min(x, y);
+
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+    }
+}
